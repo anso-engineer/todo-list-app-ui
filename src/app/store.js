2635f1cd9b11039c6bb256609047f6b5dc969368
Andrii Sotnikov <andrew.sotnikov@zoho.com>
2025-04-13 09:57:03 +0300

    Added to the store.

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from "../feature/tasks/taskSlice.js"
 import newTaskModalReducer from "../feature/newTaskModal/newTaskModalSlice.js"
 import newTaskTemplateModalReducer from "../feature/newTaskTemplateModal/newTaskTemplateModalSlice.js"
+import templateMainModalReducer from "../feature/templateMainModal/templateMainModalSlice.js"
 
 export const store = configureStore({
     reducer: {
         task: taskReducer,
         newTaskModal: newTaskModalReducer,
-        newTaskTemplateModal: newTaskTemplateModalReducer
+        newTaskTemplateModal: newTaskTemplateModalReducer,
+        templateMainModal: templateMainModalReducer
     },
 })
